f989faa4fcc8d6fa05f27375f38877ca64218788
jackie-maxx <104404240+jackie-maxx@users.noreply.github.com>
2024-09-06 16:14:08 +0700

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,7 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({
-  path: path.resolve(process.cwd(), process.env.ENV_FILE || ".env.development"),
-});
-
-const isProd = process.env.NEXT_PUBLIC_ENVIRONMENT === "development";
-
-console.log("FILE_NAME:", process.env.NEXT_PUBLIC_FILE_NAME);
-console.log("MAIN_URL:", process.env.NEXT_PUBLIC_MAIN_URL);
-console.log("ENVIRONMENT:", process.env.NEXT_PUBLIC_ENVIRONMENT);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: isProd ? null : "export",
-  basePath: isProd ? "/flipflop-web-portal" : "",
-  assetPrefix: isProd ? "/flipflop-web-portal/" : "",
-  distDir: "clients/share/build",
-  experimental: {
-    appDir: true,
-  },
+  output:  "export",
   images: {
     remotePatterns: [
       {
