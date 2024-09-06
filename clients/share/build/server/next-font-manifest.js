27a89d2f58b4a62eaa02effa1d5fdbf65c2e729b
jackie-maxx <104404240+jackie-maxx@users.noreply.github.com>
2024-09-06 15:40:59 +0700

    upload

---
+++ clients/share/build/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Volumes/DATA/Next JS Projects/flipflop-web-portal/src/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
